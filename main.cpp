5beefbda1a1aae040b2b01d354fb08c6120ede9f
MuhammadAliKhan1 <khanali198361@gmail.com>
2019-06-23 20:05:44 +0500

    Added input function

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
+int getInput()
+{
+    int n;
+    std::cout << "Input a number: ";
+    std::cin >> n;
+    return n;
+}
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
 }
